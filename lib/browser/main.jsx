e611f25bdf4a03815648a02ca4a8ed56446adb0a
Atul Varma <varmaa@gmail.com>
2015-05-07 12:05:08 -0400

    add /:repo/:owner/:locale views.

--- lib/browser/main.jsx
+++ lib/browser/main.jsx
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var React = require('react/addons');
 var ReactIntl = require('react-intl');
 var Router = require('react-router');
@@ -5,6 +6,11 @@ var bs = require('react-bootstrap');
 
 var auth = require('./auth');
 var messages = require('val!./messages');
+var githubRequest = require('../github-request');
+
+var authGithubRequest = function(method, path) {
+  return githubRequest(method, path, auth.getAccessToken());
+};
 
 var LocalizedMsg = React.createClass({
   mixins: [ReactIntl.IntlMixin],
@@ -79,7 +85,20 @@ var NotFound = React.createClass({
 
 var Home = React.createClass({
   render: function() {
-    return <p>TODO: Fill this in.</p>;
+    // TODO: Don't hardcode these links.
+
+    return (
+      <div>
+        <ul>
+          <li>
+            <Router.Link to="repo_home" params={{
+              owner: 'toolness',
+              repo: 'github-l10n-fun'
+            }}>toolness/github-l10n-fun</Router.Link>
+          </li>
+        </ul>
+      </div>
+    );
   }
 });
 
@@ -114,10 +133,137 @@ var CompleteLogin = React.createClass({
   }
 });
 
+var Repo = React.createClass({
+  mixins: [Router.State],
+  render: function() {
+    var params = this.getParams();
+
+    return (
+      <div>
+        <h1>{params.owner}/{params.repo}</h1>
+        <Router.RouteHandler/>
+      </div>
+    );
+  }
+});
+
+var RepoHome = React.createClass({
+  mixins: [Router.State],
+  getInitialState: function() {
+    return {
+      locales: []
+    };
+  },
+  fetchLocales: function() {
+    var params = this.getParams();
+
+    authGithubRequest('GET', '/repos/' + params.owner +
+                             '/' + params.repo + '/contents/locale')
+      .end(function(err, res) {
+        if (err) {
+          // TODO: Actually do something user-friendly.
+          throw err;
+        }
+        // TODO: Test all kinds of edge cases here.
+        var locales = res.body.map(function(info) {
+          var match = info.name.match(/^(.+)\.json$/);
+          return match[1];
+        });
+        this.setState({locales: locales});
+      }.bind(this));
+  },
+  componentDidMount: function() {
+    this.fetchLocales();
+  },
+  render: function() {
+    var params = this.getParams();
+
+    return (
+      <div>
+        <ul>
+          {this.state.locales.map(function(locale) {
+            return (
+              <li key={locale}>
+                <Router.Link to="repo_locale" params={_.extend({
+                  locale: locale
+                }, params)}>{locale}</Router.Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+});
+
+var RepoLocale = React.createClass({
+  mixins: [Router.State],
+  getInitialState: function() {
+    return {
+      messages: {}
+    };
+  },
+  fetchLocale: function() {
+    var params = this.getParams();
+
+    authGithubRequest('GET', '/repos/' + params.owner +
+                             '/' + params.repo +
+                             '/contents/locale/' + params.locale + '.json')
+      .end(function(err, res) {
+        if (err) {
+          // TODO: Actually do something user-friendly.
+          throw err;
+        }
+        // TODO: Test all kinds of edge cases here.
+        var messages = JSON.parse(
+          new Buffer(res.body.content, 'base64').toString('utf-8')
+        );
+        this.setState({messages: messages});
+      }.bind(this));
+  },
+  componentDidMount: function() {
+    this.fetchLocale();
+  },
+  render: function() {
+    var params = this.getParams();
+    var messages = this.state.messages;
+
+    return (
+      <div>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Message ID</th>
+              <th><a target="_blank" href="http://formatjs.io/guides/message-syntax/">ICU MessageFormat</a> Content</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(messages).map(function(name) {
+              return (
+                <tr key={name}>
+                  <td><code>{name}</code></td>
+                  <td>{messages[name]}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <Router.Link to="repo_home" params={params} className="btn btn-default">
+          <span className="glyphicon glyphicon-menu-left"/> Back
+        </Router.Link>
+      </div>
+    );
+  }
+});
+
 var routes = (
   <Router.Route handler={App} path="/">
     <Router.DefaultRoute handler={Home}/>
     <Router.Route name="oauth2_callback" path="callback" handler={CompleteLogin}/>
+    <Router.Route path="/:owner/:repo/" handler={Repo}>
+      <Router.DefaultRoute name="repo_home" handler={RepoHome}/>
+      <Router.Route name="repo_locale" path=":locale/" handler={RepoLocale}/>
+    </Router.Route>
     <Router.NotFoundRoute handler={NotFound}/>
   </Router.Route>
 );
